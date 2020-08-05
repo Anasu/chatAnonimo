a8c4a3f9b72196aac8b4793ade972895c932768d
anasu <misushima@gmail.com>
2020-08-05 16:54:21 -0400

    Se agrega chat input, que entrega datos del input hacia el state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, {Component} from 'react'
-import logo from './logo.svg';
-import './App.css';
+import ChatInput from './components/ChatInput';
 
-class App extends Component {
+class App extends Component 
+{
+  state = {
+    messages: [],
+  }
 
-  render() {
+
+  handleSubmit = (message) => {
+    this.setState({
+      messages: [...this.state.messages, message]
+    })
+  }
+
+  render() 
+  {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <ChatInput handleSubmit={this.handleSubmit}/>
       </div>
     );
   }
---
+++ src/components/ChatInput.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react'
+import { InputGroup, FormControl } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+
+
+class ChatInput extends Component
+{
+  constructor(props) 
+  {
+    super(props);
+    
+    this.initialState = {
+        input: ''
+    };
+
+    this.state = this.initialState;
+  }
+
+  handleChange = event => {
+    const { value } = event.target;
+
+    this.setState({
+        'input' : value
+      });
+  }
+
+  submitMsg = () => {
+    this.props.handleSubmit(this.state);
+    this.setState(this.initialState);
+  }
+
+  render()
+  {
+    return (
+      <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+
+          <InputGroup.Text id="basic-addon1">...</InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl
+          placeholder="Escribe algo aquí..."
+          aria-label="Input"
+          aria-describedby="basic-addon1"
+          onChange={this.handleChange}
+        />
+        <Button 
+          variant="primary" 
+          type="submit" 
+          onClick={this.submitMsg}
+          >Primary</Button>
+      </InputGroup>
+    )
+  }
+}
+
+export default ChatInput
